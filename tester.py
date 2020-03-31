4025596ff4933698490a01da801d1e728619ae29
ssalihah <46809944+ssalihah@users.noreply.github.com>
2020-03-31 16:46:26 +0800

    Update tester.py

--- tester.py
+++ tester.py
@@ -10,30 +10,32 @@ stud_list = [["salihahmr.2018@sis.smu.edu.sg","Siti Salihah",["SMT203","SMT202"]
 course_list = [] #fill in these with nested list of course_details in the format (course_code, course_name)
 skill_list = [] # fill in these with nested list of skill_details in the format (skill_name, skill_type)
 
-def test_create(email,fullname,courses):
+def test_createstudent(email,fullname,courses):
     json = {'email':email,'fullname':fullname, 'courses':courses}
     r = requests.post(create_url, json=json)
     print(r.status_code)
     print(r.text)
 
-def test_create2(course_code,course_name):
+def test_createcourse(course_code,course_name):
     json = {'course_code':course_code,'course_name':course_name}
     r=requests.post(create2_url, json=json)
     print(r.status_code)
     print(r.text)
 
-def test_create3(skill_name,skill_type):
+def test_createskill(skill_name,skill_type):
     json = {'skill_name':skill_name, 'skill_type':skill_type}
     r = requests.post(create3_url, json=json)
     print(r.status_code)
     print(r.text)
 
 
+
+
 for i in stud_list:
-    test_create(i[0],i[1],i[2])
+    test_createstudent(i[0],i[1],i[2])
 
 # for i in course_list:
-#     test_create2(i[0],i[1])
+#     test_createcourse(i[0],i[1])
 
 # for i in skill_list:
-#     test_create3(i[0],i[1])
+#     test_createskill(i[0],i[1])
