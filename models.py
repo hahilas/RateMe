8d0872ba8da41d145dc0b22116da6a980cda3b2d
ssalihah <46809944+ssalihah@users.noreply.github.com>
2020-04-10 13:03:21 +0800

    Update models.py

--- models.py
+++ models.py
@@ -18,14 +18,14 @@ class Student(db.Model):
     email = db.Column(db.String(80), unique=True, nullable=False)
     fullname = db.Column(db.String(80), nullable=False)
 
-    #many-to-many for courses
-    courses = db.relationship('Course', secondary=candidature_table, back_populates='student_course', cascade='all', lazy=True)
-
     #many-to-many for skills
     skills = db.relationship('Skill', secondary=candidature_skill_table, back_populates='student_skill', cascade='all', lazy=True)
 
+    #many-to-many for courses
+    courses = db.relationship('Course', secondary=candidature_table, back_populates='student_course', cascade='all', lazy=True)
+
     #one-to-many for rating
-    rating = db.relationship('Rating', back_populates='student_rating', uselist=True, cascade='all', lazy=True)
+    rating = db.relationship('Rating', back_populates='student_rating', uselist=True, lazy=True)
 
     def __init__(self,email,fullname):
         self.email = email
@@ -36,23 +36,23 @@ class Student(db.Model):
     
     def __repr__(self):
         return '<{} {} {} {} {}>'.format(self.id, self.email, self.fullname, self.courses,self.skills)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "fullname": self.fullname,
+            "courses": [i.course_code for i in self.courses],
+            "skills": [i.skill_name for i in self.skills],
+            "rating": [i.serialize() for i in self.rating]
+        }
 
-
-    # def serialize(self):
-    #     return {
-    #         'id':self.id,
-    #         'email':self.email,
-    #         'fullname':self.fullname,
-    #         'courses':self.courses,
-    #         'skills':self.skills,
-    #         'rating':self.rating
-    #     }
 
 class Course(db.Model):
     __tablename__='course_table'
 
     id = db.Column(db.Integer, primary_key=True)
-    course_code = db.Column(db.String(10), unique=True, nullable=False)
+    course_code = db.Column(db.String(20), unique=True, nullable=False)
     course_name = db.Column(db.String(80), unique=True, nullable=False)
 
     student_course = db.relationship('Student', secondary=candidature_table, back_populates='courses')
@@ -64,6 +64,13 @@ class Course(db.Model):
 
     def __repr__(self):
         return '<{} {} {} {}>'.format(self.id, self.course_code, self.course_name, self.student_course)
+    
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'course_code': self.course_code,
+            'course_name': self.course_name
+        }
 
 
 class Skill(db.Model):
@@ -82,25 +89,93 @@ class Skill(db.Model):
 
     def __repr__(self):
         return '<{} {} {} {}>'.format(self.id, self.skill_name, self.skill_type, self.student_skill)
+    
+    def serialize(self):
 
+        return {
+            "skill_name": self.skill_name,
+            "skill_type": self.skill_type
+        }
 
 class Rating(db.Model):
     __tablename__='rate_table'
 
-    id = db.Column(db.Integer)
-    ratee_id = db.Column(db.Integer, db.ForeignKey('student_table.id'), primary_key=True)
-    skill_id = db.Column(db.Integer, db.ForeignKey('skill_table.id'), primary_key=True)
-    rater_id = db.Column(db.Integer, primary_key=True)
-    course_code = db.Column(db.String(10), nullable=False)
+    id = db.Column(db.Integer,primary_key=True)
+    ratee_id = db.Column(db.Integer, db.ForeignKey('student_table.id'))
     timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
 
+    rate_details = db.relationship('Rate_Detail',back_populates='rates', uselist=True, cascade='all', lazy=True)
     student_rating = db.relationship('Student', back_populates='rating')
 
-    def __init__(self,ratee_id,skill_id,rater_id,course_code):
+    def __init__(self,ratee_id):
+        self.ratee_id = ratee_id
+        self.rate_details = []
+
+    def serialize(self):
+        return{
+            'id': self.id,
+            'timestamp': self.timestamp,
+            'rate_details':[i.serialize() for i in self.rate_details]
+        }
+
+class Rate_Detail(db.Model):
+    __tablename__='rate_det'
+
+    ratee_id = db.Column(db.Integer, primary_key=True)
+    skill_id = db.Column(db.Integer, primary_key=True)
+    rater_id = db.Column(db.Integer, primary_key=True)
+    course_code = db.Column(db.String(30), nullable=False)
+    rate_id = db.Column(db.Integer, db.ForeignKey('rate_table.id'))
+
+    rates = db.relationship('Rating', back_populates='rate_details')
+
+    def __init__(self,ratee_id,skill_id,rater_id,course_code,rate_id):
         self.ratee_id = ratee_id
         self.skill_id = skill_id
         self.rater_id = rater_id
         self.course_code = course_code
-    
+        self.rate_id = rate_id
+
     def __repr__(self):
-        return '<{} {} {} {} {}>'.format(self.id, self.ratee_id, self.skill_id, self.rater_id,self.course_code)
+        return '<{} {} {} {} {}>'.format(self.id, self.ratee_id, self.skill_id, self.rater_id,self.course_code,self.rate_id)
+    
+    def serialize(self):
+        skill = Skill.query.filter_by(id=self.skill_id).first()
+        rater = Student.query.filter_by(id=self.rater_id).first()
+
+        return {
+            'skill': skill.skill_name,
+            'rater': rater.email,
+            'course_code': self.course_code,
+        }
+
+class States(db.Model):
+    __tablename__='state'
+
+    id = db.Column(db.Integer, primary_key=True)
+    chatid = db.Column(db.Integer)
+    stage = db.Column(db.String(50), nullable=False)
+    ratee_email1 = db.Column(db.String(80),nullable=True)
+    course_code1= db.Column(db.String(50),nullable=True)
+    rate_skill1 = db.Column(db.String(100),nullable=True)
+    rater_email1 = db.Column(db.String(80),nullable=True)
+
+    def __init__(self,chatid,stage,ratee_email1,course_code1,rate_skill1,rater_email1):
+        self.chatid = chatid
+        self.stage = stage
+        self.ratee_email1 = ratee_email1
+        self.course_code1 = course_code1
+        self.rate_skill1 = rate_skill1
+        self.rater_email1 = rater_email1
+
+    
+    def serialize(self):
+        return {
+            'chat_id':self.chatid,
+            'stage':self.stage,
+            'ratee_email': self.ratee_email1,
+            'course_code': self.course_code1,
+            'rate_skill1': self.rate_skill1,
+            'rater_email1': self.rater_email1
+        }
+
