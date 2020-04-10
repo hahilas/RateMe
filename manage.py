3c3a0d9cc8ecea7e0a44ff3e3d3e51d9d2874fdc
ssalihah <46809944+ssalihah@users.noreply.github.com>
2020-04-10 13:03:13 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,3 @@
-
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
@@ -12,6 +11,3 @@ manager.add_command("runserver", Server(host='127.0.0.1', port=5000))
 
 if __name__ == '__main__':
     manager.run()
-
-
-
